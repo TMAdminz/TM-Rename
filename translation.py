b3a683c70f2e8ed0b0fc9643312ddbab8a8bee9c
TMAdminz <71258154+TMAdminz@users.noreply.github.com>
2020-10-14 13:26:12 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,66 @@
+class Translation(object):
+    START_TEXT = """Hello! <i><b>{}</b></i>,
+
+Your are Using Telegram Best Rename Bot <a href='https://t.me/AI_RenameBot'><b>✒️AI RENAME BOT🖋</b></a> bot
+
+I Can rename ✍ with custom thumbnail and upload as video/file
+
+I am Very Happy to Say We Finally Increased Our Bot Speed , If You Need This Service Kindly Donate Us, Using /donate Thank You
+
+Type /help for more details."""
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
+    DOWNLOAD_START_VIDEO = "**Downloading Started**"
+    DOWNLOAD_START = "Downloading Started"
+    UPLOAD_START_VIDEO = "**Uploading Started️**"
+    UPLOAD_START = "**Uploading Started️**"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Join](https://t.me/SonyMovies_Tamil).**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/MonkAno'>AI_RenameBot</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved 🤗 permanently. This image will be used in the File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "🤔 Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "🤨 Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "👹 Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support \n\n@SerialCoIn</b>"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "😠 No Custom ThumbNail found."
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """Hai <b><i>{}</i></b>, 
+
+I am AI RenameBot ✍ by <a href='https://t.me/MonkAno'><b>MonkAno</b></a>
+    
+1. Send Me A Thumbnail.
+
+2. Send me the file to be Renamed.
+
+3. Reply to that message with <code>/rename new name.extension</code>. with custom thumbnail support.\n(upload as file)
+
+4. Reply to that message with <code>/rename_vidoe new name.extension</code>. with custom thumbnail support.\n(uploading as Video)
+
+--------
+
+Movie Channel : <b>@SonyPictures_Tamil</b>"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "🙆 Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🙋 Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@AI_RenameBot</code>
+Please short your file name and try again!"""
+
+    About = """Hi __{}__,\n
+**📝 Language:** Python 3
+
+**🧰 Framework:** Pyrogram
+
+**👨‍💻 Developer:** [MonkAno](https://t.me/MonkAno)
+
+**📮 Channel:** [SonyMovies Tamil](https://t.me/SonyMovies_Tamil)
+
+**📮 Channel:** [SonyPictures Tamil](https://t.me/SonyPictures_Tamil)
+
+**💻 Source Code:**[Here](https://gplinks.co/RenameSource)"""
